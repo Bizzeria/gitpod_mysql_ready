eff279ba42e2c2ae59f06896be16dc831d3ffac4
Bizzeria <zungrone.lorenzo@iisgalvanimi.edu.it>
2022-12-21 12:00:22 +0000

    versione rusci 3

---
+++ bizzasql3.py
@@ -0,0 +1,76 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")                                     #3.1 riempire una tabella in mysql; inserire una riga nella tabella customer
+#record=elemento di un database relazionale caratterizzato da uno o più attributi                                                                               
+#mydb.commit deve attuare un cambiamento 
+                                                                                 
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)   #mycursor.executemany(sql, NOME LISTA DI RECORDS)
+
+mydb.commit()
+
+print(mycursor.rowcount, "was inserted.")                                        #3.2  inserire più righe in una tabella con executemany  
+
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("Michelle", "Blue Village")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print("1 record inserted, ID:", mycursor.lastrowid)                             #3.3 riporta l' ID di una riga, se richiesti più di uno ridarà solo l'ultima richiesta 
+                                                                                                                                                               
